c573c856207d1846a925a9d88abb3917ba47ffea
Ryan1729 <Ryan1729@gmail.com>
2021-10-31 01:06:00 -0600

    generate bindings with a script, so bindgen is not a build dep.
    Yes, if the header changes you'll need it, but that seems unlikely.

--- stbhw/src/sys.rs
+++ stbhw/src/sys.rs
@@ -1,15 +1,10 @@
-#![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-#![allow(clippy::redundant_static_lifetimes)]
-// Shouldn't need this once bindings to stuff we don't need are gone.
-#![allow(improper_ctypes)]
-// Shouldn't need this once bindings to stuff we don't need are gone.
-#![allow(deref_nullptr)]
-// Shouldn't need this once bindings to stuff we don't need are gone.
 #![allow(unused)]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+mod bindings;
+
+use bindings::*;
 
 use std::ffi::CStr;
 
